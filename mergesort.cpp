e550127ac6901a7465a1ac1fc070154805009413
applezone <2188563245@qq.com>
2025-12-26 19:02:16 +0800

    first commit

---
+++ mergesort.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+const int N = 1e6 + 10;
+int n;
+int q[N], tmp[N];
+
+void merge_sort(int *q, int l, int r)
+{
+    if (l >= r)
+        return;
+    if (l + 1 == r)
+    {
+        if (q[l] > q[r])
+        {
+            swap(q[l], q[r]);
+            return;
+        }
+    }
+    merge_sort(q, l, (l + r) / 2);
+    merge_sort(q, (l + r) / 2 + 1, r);
+    int k = 0, i = l, j = (l+r)/2+1;
+    while (i <= (l + r) / 2 && j <= r)
+    {
+        if (q[i] < q[j])
+        {
+            tmp[k++] = q[i++];
+        }
+        else
+        {
+            tmp[k++] = q[j++];
+        }
+    }
+    while (i <= (l + r) / 2)
+        tmp[k++] = q[i++];
+    while (j <= r)
+        tmp[k++] = q[j++];
+    for (i = l, j = 0; i <= r; i++, j++) 
+        q[i] = tmp[j];
+}
+
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &q[i]);
+    }
+    merge_sort(q, 0, n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", q[i]);
+    }
+    return 0;
+}
