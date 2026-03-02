51a53bd1bbf806bc66eca443a86d8867477b0717
apple-zone <2188563245@qq.com>
2026-03-02 20:00:23 +0800

    哈希表完成

---
+++ 模拟散列表.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+const int N = 100010;
+
+int h[N],e[N];
+
+void insert(int x)
+{
+    int k = (x%N + N)% N;
+
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    memset(h,-1,sizeof(h));
+
+    while(n--)
+    {
+        char op[2];
+        int x;
+        scanf("%s%d",op,&x);
+
+        if(*op =='I')
+        {
+            int y;
+            scanf("%d",&y);
+            insert(x,y);
+        }
+    }
+}
